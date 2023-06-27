9b09703ff99506695df104e7b5a51dc6691aac0f
Pandora <nimfanefor@gmail.com>
2023-06-27 15:11:06 +0600

    Подключил url, изменил html шаблоны

--- online_store/webapp/views.py
+++ online_store/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.models import Good
 
@@ -26,12 +26,12 @@ def good_create_view(request):
 
 
 def good_view(request, *args, pk, **kwargs):
-    good = Good.objects.get(id=pk)
+    good = get_object_or_404(Good, id=pk)
     return render(request, "good.html", {"good": good})
 
 
 def delete_good(request):
     good_id = request.GET.get("id")
-    good = Good.objects.get(id=good_id)
+    good = get_object_or_404(Good, id=good_id)
     good.delete()
     return redirect('good_list')
