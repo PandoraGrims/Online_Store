2001197ee6901ec9f91b889b2a5e49a6fb09705c
Pandora <nimfanefor@gmail.com>
2023-07-04 18:52:01 +0600

    обавил возможность редактировать и удалять

--- online_store/webapp/urls.py
+++ online_store/webapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from webapp.views import good_list_view, good_create_view, good_view, delete_good, category_add_view, category_view
+from webapp.views import good_list_view, good_create_view, good_view, delete_good, category_add_view, category_view, \
+    good_update
 
 urlpatterns = [
     path('', good_list_view, name="index"),
     path('goods/', good_list_view, name="index"),
     path('good/add/', good_create_view, name="good_add"),
     path('good/<int:pk>', good_view, name="good_view"),
-    path('delete/<int:good_id>/', delete_good, name="delete_good"),
+    path('delete_good/<int:pk>/', delete_good, name="delete_good"),
+    path('good_update/<int:pk>/', good_update, name="good_update"),
     path('categories/add/', category_add_view, name='category_add'),
     path('categories/', category_view, name='category_view'),
 ]
--- online_store/webapp/views.py
+++ online_store/webapp/views.py
@@ -11,22 +11,30 @@ def good_list_view(request):
     return render(request, "index.html", context)
 
 
-# def good_create_view(request):
-#     if request.method == "GET":
-#         categories = Category.objects.all()
-#         return render(request, "create_good.html", {"categories": categories})
-#     else:
-#         category = get_object_or_404(Category, pk=request.POST.get("category"))
-#         good = Good.objects.create(
-#             title=request.POST.get("title"),
-#             description=request.POST.get("description"),
-#             category=category,
-#             price=request.POST.get("price"),
-#             created_at=request.POST.get("created_at"),
-#             image_url=request.POST.get("image_url"),
-#         )
-#
-#     return redirect("good_view", pk=good.pk)
+def good_update(request, pk):
+    good = get_object_or_404(Good, id=pk)
+    if request.method == "GET":
+        form = GoodForm(initial={"title": good.title,
+                                 "category": good.category,
+                                 "description": good.description,
+                                 "price": good.price,
+                                 "remainder": good.remainder,
+                                 "image_url": good.image_url,
+                                 })
+        return render(request, "good_update.html", {"form": form})
+    else:
+        form = GoodForm(data=request.POST)
+        if form.is_valid():
+            good.title = request.POST.get("title")
+            good.category = request.POST.get("category")
+            good.description = request.POST.get("description")
+            good.price = request.POST.get("price")
+            good.remainder = request.POST.get("remainder")
+            good.image_url = request.POST.get("image_url")
+            good.save()
+            return redirect("good")
+        else:
+            return render(request, "good_update.html", {"form": form})
 
 
 def good_create_view(request):
@@ -60,10 +68,13 @@ def category_view(request):
     return render(request, "category_view.html", context)
 
 
-def delete_good(request, good_id):
-    good = get_object_or_404(Good, pk=good_id)
-    context = {"good": good}
-    return render(request, "index.html", context)
+def delete_good(request, pk):
+    good = get_object_or_404(Good, id=pk)
+    if request.method == "GET":
+        return render(request, "delete_good.html", {"good": good})
+    else:
+        good.delete()
+        return redirect("index")
 
 
 def category_add_view(request):
